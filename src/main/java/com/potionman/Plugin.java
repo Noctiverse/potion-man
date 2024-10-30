7c65920f381de1ab3ab0a91a18eb46b4a68f5ae2
noctiverse <noctiverse@gmail.com>
2024-10-29 23:38:53 -0600

    V1

---
+++ src/main/java/com/potionman/Plugin.java
@@ -0,0 +1,42 @@
+package com.potionman;
+
+import java.io.*;
+import javax.sound.sampled.*;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.api.events.NpcSpawned;
+import net.runelite.api.NPC;
+import net.runelite.api.NpcID;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.PluginDescriptor;
+
+@Slf4j
+@PluginDescriptor(
+	name = "Potion Man"
+)
+public class Plugin extends net.runelite.client.plugins.Plugin
+{
+	@Inject
+	private Client client;
+
+	@Subscribe
+	public void onNpcSpawned(final NpcSpawned event)
+	{
+		final NPC npc = event.getNpc();
+
+		if (npc.getId() == NpcID.DR_JEKYLL || npc.getId() == NpcID.DR_JEKYLL_314)
+		{
+			try
+			{
+				Clip clip = AudioSystem.getClip();
+				clip.open(AudioSystem.getAudioInputStream(new File("src/main/java/com/potionman/potion man.wav")));
+				clip.start();
+			}
+			catch (Exception exc)
+			{
+				exc.printStackTrace(System.out);
+			}
+		}
+	}
+}
--- src/test/java/com/potionman/PluginLauncher.java
+++ src/test/java/com/potionman/PluginLauncher.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.potionman;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PluginLauncher
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(Plugin.class);
 		RuneLite.main(args);
 	}
 }
